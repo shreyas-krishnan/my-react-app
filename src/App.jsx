503ad3faabf0c8e5a5dd56ce6c0eacf677a7f93e
Shreyas Krishnan <k.shreyas@wustl.edu>
2026-04-02 15:13:48 -0500

    allow Square to receive and display a value

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
-
-
 
 export default function Board() {
   return (
-   <>
+    <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
         <button className="square">4</button>
