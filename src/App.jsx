5a34c8cb29055ff96863bee30c1ede25b34fc1d6
Shreyas Krishnan <k.shreyas@wustl.edu>
2026-04-02 15:47:30 -0500

    add unique keys to move history

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
