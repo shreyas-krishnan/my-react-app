18f6e9b2a9186efbfd67a123014fc74b55629ee0
Shreyas Krishnan <k.shreyas@wustl.edu>
2026-04-02 15:26:18 -0500

    pass squares state from Board to Square components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { useState } from 'react';
 
-function Square() { // Removed { value } prop
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X'); 
-  }
-
+function Square({ value }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square">
       {value}
     </button>
   );
@@ -16,23 +10,25 @@ function Square() { // Removed { value } prop
 
 
 export default function Board() {
+ 
   const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
