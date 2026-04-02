5d5f9e7a472e89699ec62d0b012abc52f5fac56f
Shreyas Krishnan <k.shreyas@wustl.edu>
2026-04-02 15:29:29 -0500

    update handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick() {
-    const nextSquares = squares.slice(); 
-    nextSquares[0] = "X";
-    setSquares(nextSquares);
-  }
+  function handleClick(i) {
+  const nextSquares = squares.slice();
+  nextSquares[i] = "X";
+  setSquares(nextSquares);
+}
 
   return (
     <>
