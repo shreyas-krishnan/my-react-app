c7e351bf7304801f065709a405756e20bace3f0c
Shreyas Krishnan <k.shreyas@wustl.edu>
2026-04-02 15:14:23 -0500

    convert all board rows to use the Square component

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ export default function Board() {
         <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
