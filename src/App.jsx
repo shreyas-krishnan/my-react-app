7a4a103adc3449832a25b1f88ac5d9b029b03312
Shreyas Krishnan <k.shreyas@wustl.edu>
2026-04-02 15:17:19 -0500

    updated console log click handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
+
 function Square({ value }) {
   function handleClick() {
     console.log('clicked!');
   }
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button 
+      className="square" 
+      onClick={handleClick}
+    >
       {value}
     </button>
   );
 }
+
 
 export default function Board() {
   return (
